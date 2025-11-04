1380e09807cd5b275ee1f836161afe05bef0760f
matyoqubov1brohim <ibush0925@gmail.com>
2025-11-05 00:37:55 +0500

    created info component for  row movies

---
+++ src/components/row-movies-item/row-movies-item.jsx
@@ -0,0 +1,21 @@
+import './row-movies-item.scss'
+
+const RowMoviesItem = ({ movie, onToggleOpen }) => {
+	return (
+		<div className='movieitem' onClick={onToggleOpen}>
+			<img src={movie.image} alt={movie.title} />
+
+			<h2>
+				{movie.title} {movie.index + 1}
+			</h2>
+
+			<div className='movieitem-descr'>
+				<p>{movie.date}</p>
+				<div className='dot' />
+				<p>{movie.duration}m</p>
+			</div>
+		</div>
+	)
+}
+
+export default RowMoviesItem
