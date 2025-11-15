4d8cb1fff181498c48c34c63f7ea61102ddb5860
ibrohimmatyoqubov <ibush0925@gmail.com>
2025-11-15 20:13:49 +0500

    import movies from API, add responsive styles, ErrorBoundary, and footer; fix bugs, Lifecycle hooks

--- src/components/error/error.jsx
+++ src/components/error/error.jsx
@@ -3,7 +3,7 @@ import './error.scss'
 const Error = () => {
 	return (
 		<div className='center'>
-			<img src='/error.gif' alt='error' />
+			<img src='/error.gif' alt='error' className='errormessage' />
 		</div>
 	)
 }
